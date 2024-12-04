67ef050a29eb7a4a5c70308d1fc8d16e6fd4732f
Simon Théorêt <simonteoret@hotmail.com>
2024-12-03 22:43:50 -0500

    uv

--- data/hello.py
+++ data/hello.py
@@ -1,8 +1,20 @@
 import argparse
-import 
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+
 
 def main():
-    print("Hello from data!")
+    tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+    model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+    nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+    data = load_dataset("eriktks/conll2003")
+    lists = []
+    sets = ["train", "test", "validation"]
+    for set_instance in sets:
+        dataset = data[set_instance]
+        for example in dataset:
+            print(example)
+            lists.append(example.tokens, example.ner_tags)
 
 
 if __name__ == "__main__":
