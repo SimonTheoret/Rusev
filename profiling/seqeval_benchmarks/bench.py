c80b3d91252c6d1eb03ed55cfddf633774405030
Simon Théorêt <simonteoret@hotmail.com>
2024-12-07 22:42:47 -0500

    benchs are out

--- profiling/seqeval_benchmarks/bench.py
+++ profiling/seqeval_benchmarks/bench.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from argparse import ArgumentParser
-from seqeval.metrics import classification_report
+from time import time
+from seqeval.metrics.v1 import classification_report
+from seqeval.scheme import IOB2
 from typing import Self
 import jsonlines
 
@@ -28,10 +30,33 @@ def build_lists(path: str) -> tuple[list[list[str]], list[list[str]]]:
     return (true_list, pred_list)
 
 
-def main():
-    print("Hello from seqeval-benchmarks!")
+def main(n_samples: int, dataset: str):
+    total_duration: float = 0.0
+    path: str = f"./data/datasets/{dataset}_dataset.jsonl"
+    for _ in range(n_samples):
+        true_list, pred_list = build_lists(path)
+        now = time()
+        classification_report(
+            y_true=true_list,
+            y_pred=pred_list,
+            sample_weight=None,
+            digits=2,
+            output_dict=False,
+            zero_division=0,
+            suffix=False,
+            scheme=IOB2,
+        )
+        elapsed = time() - now
+        total_duration += elapsed
+    print(f"Total duration: {total_duration} with {n_samples} samples")
 
 
 if __name__ == "__main__":
-    args = ArgumentParser.parse_args()
-    main()
+    parser = ArgumentParser(
+        prog="Seqeval Benchmark",
+        description="Benchmark SeqEval on different size of datasets",
+    )
+    parser.add_argument("--n_samples", type=int, default=1)
+    parser.add_argument("--dataset", type=str, default="big")
+    args = parser.parse_args()
+    main(args.n_samples, args.dataset)
