e3bd37fe6aa74e2a5c31c2e58ba09072778ba67d
Simon Théorêt <simonteoret@hotmail.com>
2025-01-11 02:53:59 -0500

    Moved rstest to dev.deps

--- py_bindings/src/lib.rs
+++ py_bindings/src/lib.rs
@@ -1,14 +1,28 @@
 use pyo3::prelude::*;
+use rusev;
 
 /// Formats the sum of two numbers as string.
+// fn classification_report() -> PyResult<String> {
+//     Ok(String::from("Ok"))
+// }
+
+#[inline(always)]
 #[pyfunction]
-fn classification_report() -> PyResult<String> {
-    Ok(String::from("Ok"))
+pub fn classification_report<'a>(
+    y_true: Vec<Vec<&'a str>>,
+    y_pred: Vec<Vec<&'a str>>,
+    sample_weight: Option<Vec<f32>>,
+    zero_division: &'a str, // DivByZeroStrat,
+    scheme: &'a str, //SchemeType,
+    suffix: bool,
+    parallel: bool,
+) -> Result<Reporter, ComputationError<String>> {
+    todo!()
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn py_bindings(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(classification_report, m)?)?;
     Ok(())
 }
